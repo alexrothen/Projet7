e139edaec924fda3f3ce1435a44c1e61a57b19a9
alex rothenburger <alex.rothenburger@gmail.com>
2020-11-18 15:40:46 +0100

    mysql init

--- backend/models/message.js
+++ backend/models/message.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Message.belongsTo(models.User, {
+      models.Message.belongsTo(models.User, {
         foreignKey:{
           allowNull: false
         } 
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-// Imports
-const express = require('express');
-const { Sequelize } = require('sequelize/types');
-const sequelize = new Sequelize({
-    
-})
-
-const app = express();
-
